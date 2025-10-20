1cf365a85b8313b29f2411f5d4f832263aad1c6c
Dongwon Jeong <tunajjang@gmail.com>
2025-10-20 18:28:44 +0900

    express example final

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ import tasks from './mock.js';
 
 const PORT = 3000;
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello Express');
+});
+
+app.post('/tasks', (req, res) => {
+  const data = req.body;
+  const ids = tasks.map((task) => task.id);
+  const nextId = Math.max(...ids) + 1;
+  const now = new Date();
+  const newTask = {
+    ...data,
+    id: nextId,
+    createdAt: now,
+    updatedAt: now,
+    isComplete: false,
+  };
+  tasks.push(newTask);
+
+  res.status(201).send(newTask);
 });
 
 app.get('/tasks', (req, res) => {
@@ -30,6 +48,44 @@ app.get('/tasks', (req, res) => {
   }
 
   res.send(newTasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((task) => task.id === id);
+
+  if (task) {
+    res.send(task);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id' });
+  }
+});
+
+app.patch('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((task) => task.id === id);
+
+  if (task) {
+    Object.keys(req.body).forEach((key) => {
+      task[key] = req.body[key];
+    });
+    task.updatedAt = new Date();
+    res.send(task);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id' });
+  }
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const taskIdx = tasks.findIndex((task) => task.id === id);
+
+  if (taskIdx !== -1) {
+    const [deletedTask] = tasks.splice(taskIdx, 1);
+    res.send(deletedTask);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id' });
+  }
 });
 
 app.listen(PORT, (err) => {
