342e21ba758125e5b94caf0f3db575cc453344d0
Dongwon Jeong <tunajjang@gmail.com>
2025-10-21 10:36:11 +0900

    add all after mongoose

---
+++ constant.js
@@ -0,0 +1,2 @@
+export const DATABASE_URL =
+  'mongodb+srv://tunajjang:HJ4e0wLnvc9qy0LA@codeit-6th-jeong-cluste.8opmf4y.mongodb.net/todo-api?retryWrites=true&w=majority&appName=codeit-6th-jeong-Cluster';
